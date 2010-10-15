5bb7960fd66e24718bd2df01e947c108ba790bb7
Rudolf Weeber <weeber@okapi.ica1.uni-stuttgart.de>
2010-10-15 12:04:37 +0200

    Comments that explain what some functions in virtual_sites_com.h do

--- virtual_sites_com.h
+++ virtual_sites_com.h
@@ -31,14 +31,18 @@ Particle *get_mol_com_particle(Particle *calling_p);
 // and returns their distance
 double get_mol_dist(Particle *p1,Particle *p2);
 
-// Seems to do just the same ???
+//Distance between molecules in the partcfg data structure
 double get_mol_dist_partcfg(Particle *p1,Particle *p2);
 
 
 // Unknown
+// Analyze the pressure on the molecule level
 int parse_and_print_pressure_mol(Tcl_Interp *interp,int argc, char **argv);
+// Analyze kinetic energy of the molecules
 int parse_and_print_energy_kinetic_mol(Tcl_Interp *interp,int argc, char **argv);
+// Sanity checks the positions of virtual sites
 int parse_and_check_mol_pos(Tcl_Interp *interp,int argc, char **argv);
+// Analze dipole moment on melecular basis
 int parse_and_print_dipole_mol(Tcl_Interp *interp,int argc, char **argv);
 #endif
 
