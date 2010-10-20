88e6112f56e6ead55fc9e72bd3a9084bfce9fa2c
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2010-10-20 16:46:31 +0200

    Virtual_sites_relative: correction to update function

--- virtual_sites_relative.c
+++ virtual_sites_relative.c
@@ -9,13 +9,52 @@
 
 #ifdef VIRTUAL_SITES_RELATIVE
 
+#include "rotation.h"
+
 // This is the "relative" implementation for virtual sites.
 // Virtual particles are placed relative to the position of a real particle
+
+// Obtain the real particle from which a virtual particle derives it's position
+// Note: for now, we use the mol_di property of Particle
+Particle* vs_relative_get_real_particle(Particle* p)
+{
+ return local_particles[p->p.mol_id];
+}
+
+
+
 
 // Update the pos of the given virtual particle as defined by the real 
 // particles in the same molecule
 void update_mol_pos_particle(Particle *p)
-{}
+{
+ // First obtain the real particle responsible for this virtual particle:
+ // Find the 1st real particle in the topology for the virtual particle's mol_id
+ Particle *p_real = vs_relative_get_real_particle(p);
+ // Check, if a real particle was found
+ if (!p_real)
+ {
+  fprintf(stderr,"virtual_sites_relative.c - update_mol_pos_particle(): No real particle associated with virtual site.\n");
+  return;
+ }
+ 
+ // Calculate the quaternion defining the orientation of the vecotr connectinhg
+ // the virtual site and the real particle
+ // This is obtained, by multiplying the quaternion representing the director
+ // of the real particle with the quaternion of the virtual particle, which 
+ // specifies the relative orientation.
+ double q[4];
+ multiply_quaternions(p->r.quat,p_real->r.quat,q);
+ // Calculate the director resulting from the quaternions
+ double director[3];
+ convert_quat_to_quatu(q,director);
+ 
+ // Calculate the new position of the virtual sites from
+ // position of real particle + director 
+ int i;
+ for (i=0;i<3;i++)
+  p->r.p[i] =p_real->r.p[i] +director[i];
+}
 
 // Update the vel of the given virtual particle as defined by the real 
 // particles in the same molecule
