0998f6aa848248d6890f8b06e229a263632ec474
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2010-11-09 14:57:40 +0100

    virtual_sites_relative: Tentative fix for periodic boundary problems. Still requires skin=0

--- virtual_sites_relative.c
+++ virtual_sites_relative.c
@@ -58,6 +58,8 @@ void update_mol_pos_particle(Particle *p)
  for (i=0;i<3;i++)
  {
   p->r.p[i] =p_real->r.p[i] +director[i]/l*p->p.vs_relative_distance;
+//  p->l.i[i] =p_real->l.i[i];
+  fold_coordinate(p->r.p, p->l.i, i);
  }
 }
 
